c51f11afd100a1d6617ce0e34149e7e540ec4d50
jae1 <7904001@gmail.com>
2020-07-29 22:06:38 -0700

    Adds sign up form

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,12 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class QuestionForm(FlaskForm):
-    subject = StringField('제목', validators=[DataRequired('제목은 필수입력 항목입니다.')])
-    content = TextAreaField('내용', validators=[DataRequired('내용은 필수입력 항목입니다.')])
-
+    subject = StringField('Subject', validators=[DataRequired('Subject is required.')])
+    content = TextAreaField('Body', validators=[DataRequired('Body is required.')])
 
 class AnswerForm(FlaskForm):
-    content = TextAreaField('내용', validators=[DataRequired('내용은 필수입력 항목입니다.')])
+    content = TextAreaField('Body', validators=[DataRequired('Body is required.')])
+
+class NewUserForm(FlaskForm):
+    email = EmailField('Email Address', [DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
+    password1 = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', 'Passwords do not match')])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired()])
